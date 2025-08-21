793af55db6ecb43815dbb167bc7d96c7ae5b0112
meganli <lovecezanne@gmail.com>
2025-08-21 16:29:02 +0800

    練習串api改成共用函式

---
+++ s20250821/data/newAQI.js
@@ -0,0 +1,143 @@
+let newAQI = [
+  {
+    "id": 1,
+    "sitename": "基隆",
+    "aqi": "30",
+    "status": "良好",
+    "publishtime": "2025/08/19 10:00:00"
+  },
+  {
+    "id": 2,
+    "sitename": "汐止",
+    "aqi": "45",
+    "status": "良好",
+    "publishtime": "2025/08/19 10:00:00"
+  },
+  {
+    "id": 3,
+    "sitename": "新店",
+    "aqi": "33",
+    "status": "良好",
+    "publishtime": "2025/08/19 10:00:00"
+  },
+  {
+    "id": 4,
+    "sitename": "土城",
+    "aqi": "53",
+    "status": "普通",
+    "publishtime": "2025/08/19 10:00:00"
+  },
+  {
+    "id": 5,
+    "sitename": "板橋",
+    "aqi": "48",
+    "status": "良好",
+    "publishtime": "2025/08/19 10:00:00"
+  },
+  {
+    "id": 6,
+    "sitename": "新莊",
+    "aqi": "44",
+    "status": "良好",
+    "publishtime": "2025/08/19 10:00:00"
+  },
+  {
+    "id": 7,
+    "sitename": "菜寮",
+    "aqi": "40",
+    "status": "良好",
+    "publishtime": "2025/08/19 10:00:00"
+  },
+  {
+    "id": 8,
+    "sitename": "林口",
+    "aqi": "44",
+    "status": "良好",
+    "publishtime": "2025/08/19 10:00:00"
+  },
+  {
+    "id": 9,
+    "sitename": "淡水",
+    "aqi": "30",
+    "status": "良好",
+    "publishtime": "2025/08/19 10:00:00"
+  },
+  {
+    "id": 10,
+    "sitename": "士林",
+    "aqi": "40",
+    "status": "良好",
+    "publishtime": "2025/08/19 10:00:00"
+  },
+  {
+    "id": 11,
+    "sitename": "中山",
+    "aqi": "35",
+    "status": "良好",
+    "publishtime": "2025/08/19 10:00:00"
+  },
+  {
+    "id": 12,
+    "sitename": "萬華",
+    "aqi": "42",
+    "status": "良好",
+    "publishtime": "2025/08/19 10:00:00"
+  },
+  {
+    "id": 13,
+    "sitename": "古亭",
+    "aqi": "41",
+    "status": "良好",
+    "publishtime": "2025/08/19 10:00:00"
+  },
+  {
+    "id": 14,
+    "sitename": "松山",
+    "aqi": "53",
+    "status": "普通",
+    "publishtime": "2025/08/19 10:00:00"
+  },
+  {
+    "id": 15,
+    "sitename": "大同",
+    "aqi": "59",
+    "status": "普通",
+    "publishtime": "2025/08/19 10:00:00"
+  },
+  {
+    "id": 16,
+    "sitename": "桃園",
+    "aqi": "43",
+    "status": "良好",
+    "publishtime": "2025/08/19 10:00:00"
+  },
+  {
+    "id": 17,
+    "sitename": "大園",
+    "aqi": "37",
+    "status": "良好",
+    "publishtime": "2025/08/19 10:00:00"
+  },
+  {
+    "id": 18,
+    "sitename": "觀音",
+    "aqi": "39",
+    "status": "良好",
+    "publishtime": "2025/08/19 10:00:00"
+  },
+  {
+    "id": 19,
+    "sitename": "平鎮",
+    "aqi": "32",
+    "status": "良好",
+    "publishtime": "2025/08/19 10:00:00"
+  },
+  {
+    "id": 20,
+    "sitename": "龍潭",
+    "aqi": "41",
+    "status": "良好",
+    "publishtime": "2025/08/19 10:00:00"
+  }
+]
+
